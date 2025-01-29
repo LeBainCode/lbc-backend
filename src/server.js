d7bb94b7cc414c28b10087d8ea8d8cf7ce0b09b7
Danlyn Medou <danbayan@protonmail.com>
2025-01-29 04:01:11 +0100

    adjusting back

--- src/server.js
+++ src/server.js
@@ -53,7 +53,7 @@ passport.use(new GitHubStrategy({
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
     callbackURL: `${process.env.NODE_ENV === 'production'
       ? 'https://lebaincode-backend.onrender.com'
-      : 'http://localhost:3000'}/api/auth/github/callback`
+      : 'http://localhost:5000'}/api/auth/github/callback`
   },
   async function(accessToken, refreshToken, profile, done) {
     try {
